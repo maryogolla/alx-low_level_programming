93e62ec9772319b399f5ed02aa31efe9b83d3e6b
maryogolla <maryogolla040@gmail.com>
2022-09-09 07:22:19 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,4 +1,5 @@
 #inside <stdio.h>
+
 /**
  *main - Entry point
  *
@@ -6,20 +7,21 @@
  */
 int main(void)
 {
-	char lower_case;
-	char upper_case;
+char lower_case;
+char upper_case;
 
-	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 {
-	putchar(lower_case);
+putchar(lower_case);
 }
 
-	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
 {
-	putchar(upper_case);
+putchar(upper_case);
 }
 
-	putchar ('\n');
+putchar ('\n');
 
-	return (0);
+return (0);
+
 }
