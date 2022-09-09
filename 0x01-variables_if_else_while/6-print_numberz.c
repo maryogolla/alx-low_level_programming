e0184b48548550fc89a5d3472094ac7ce3df96f3
maryogolla <maryogolla040@gmail.com>
2022-09-09 10:18:44 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry mode
+ *
+ *
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48 ; i <= 57; i++)
+	{
+		putchar (i);
+	}
+	putchar('\n');
+	return (0);
+}
+
