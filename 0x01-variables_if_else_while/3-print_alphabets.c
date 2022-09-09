09ea7b94b1f142965185f35e7514caeaa05ddb25
maryogolla <maryogolla040@gmail.com>
2022-09-09 05:48:33 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,21 +6,20 @@
  *Return: Always 0 (Success)
  */
 int main(void)
-{
- 	char lower_case;
-	char upper_case;
-
-	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
-{
-	putchar(upper_case);
 }
+	 char lower_case;
+	char upper_case;
 
 	for (lower_case = 'a'; lower_case <= 'a'; lower_case++)
 {
 	putchar(lower_case);
 }
 
-	putchar('\n');
+	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+{
+	putchar(upper_case);
+}
 
+	putchar('\n');
 	return (0);
 }
