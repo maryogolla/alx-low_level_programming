bb96cd7c982e4593e6a83fef4d15fe08fd42c2a0
maryogolla <maryogolla040@gmail.com>
2022-09-13 07:53:00 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,6 +6,7 @@
  * Return: Always 0.
  */
 void times_table(void)
+{
 
 int i;
 int j;
@@ -14,28 +15,28 @@ int sum;
 for (i = 0; i <= 9; i++)
 {
 	for (j = 0; j <= 9; j++)
+	{
+		sum = i * j;
+		if (sum > 9)
 		{
-			sum = i * j;
-			if (sum > 9)
-			{
-				_putchar(sum / 10 + '0');
-				_putchar(sum % 10 + '0');
-			}
-			else if (j != 0)
-			{
-				_putchar(' ');
-				_putchar(sum + '0');
-			}
-			else
-			{
-				_putchar(sum + '0');
-			}
-			if (j < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar(sum / 10 + '0');
+			_putchar(sum % 10 + '0');
 		}
+		else if (j != 0)
+		{
+			_putchar(' ');
+			_putchar(sum + '0');
+		}
+		else
+		{
+			_putchar(sum + '0');
+		}
+		if (j < 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
 	_putchar('\n');
 }
-	
+}
