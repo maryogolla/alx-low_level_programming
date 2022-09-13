44009501cc2eb1595861e656fcee4617cf9f95ad
maryogolla <maryogolla040@gmail.com>
2022-09-13 07:21:54 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,73 +8,35 @@
 void times_table(void)
 {
 
-	int a;
-	int b;
-	int c;
+	int i;
+	int j;
+	int sum;
 
-	for (a = 0; a <= 9; a++)
-{
-
-	for (b = 0; b <= 9; b++)
-
-{
-
-	c = a * b;
-
-	if ((c / 10) == 0)
-
-{
-
-	if (b == 0)
-
-{
-
-	_putchar ('0');
-
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			sum = i * j;
+			if (sum > 9)
+			{
+				_putchar(sum / 10 + '0');
+				_putchar(sum % 10 + '0');
+			}
+			else if (j != 0)
+			{
+				_putchar(' ');
+				_putchar(sum + '0');
+			}
+			else
+			{
+				_putchar(sum + '0');
+			}
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
 }
-
-	if (b != 0)
-
-{
-
-	_putchar (' ');
-	_putchar ((c % 10) + '0');
-
-}
-
-if (b < 9)
-
-{
-
-	_putchar (',');
-	_putchar (' ');
-
-}
-
-}
-
-	else
-
-{
-
-	_putchar ((c / 10) + '0');
-	_putchar ((c % 10) + '0');
-
-	if (b < 9)
-
-{
-
-	_putchar (',');
-	_putchar (' ');
-
-}
-
-}
-
-}
-
-	_putchar ('\n');
-
-}
-
-}
+	
