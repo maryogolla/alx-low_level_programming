6f9d17da3af8343c1a4252c0c8cf681d2e01429f
maryogolla <maryogolla040@gmail.com>
2022-09-13 10:06:46 -0700

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * main - main
- *
+ * main - test functions
  * Return: 0
  */
 int main(void)
 {
-int i;
-i = 0;
-positive_or_negative(i);
-return (0);
+	int i;
+
+	i = 98;
+
+	positive_or_negative(i);
+
+	return (0);
 
 }
