f6a00bba41cb8b97e8a0afa0a1e6c0687fbd6f79
maryogolla <maryogolla040@gmail.com>
2022-09-28 10:11:41 -0700

    My 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,13 +10,10 @@
 int _evaluate(int i, int n)
 {
 	/*Evaluate function*/
-
 	if (n == 0 || n == 1)
 	return (n);
-
 	else if (i * i < n)
 	return (_evaluate(i + 1, n));
-
 	else if (i * i == n) /*Condiction base*/
 	return (i);
 	return (-1);
@@ -32,6 +29,7 @@ int _evaluate(int i, int n)
 int _sqrt_recursion(int n)
 {
 	int i = 0;
+
 	if (i < 0) /*If n is negative*/
 	return (-1);
 	else
